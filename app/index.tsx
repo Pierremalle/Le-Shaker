27f1a8cc0b92b5486efb4aba99714c111231249d
Achard Lucas <pierremalle@pierremallehomelab.home>
2024-09-09 19:34:38 +0200

    implement working reusable NavLink Button

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
+import NavButton from "@/components/UiElements/NavButton";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>Home</Text>
+      <NavButton path="/(modes)" pathName="JOUER" />
     </View>
   );
 }
